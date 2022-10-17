758ab734dded12a3ed4cc6b012776116c5df130d
Zach <zesweets12@gmail.com>
2022-10-16 21:31:34 -0400

    add resources file

---
+++ resources.js
@@ -0,0 +1,10 @@
+/** 
+ * 
+ * NP: Nozzle Pressure
+ * FL: Friction loss of the length(s) of hose
+ * BP: Back pressure if going up over 10ft
+ * AFL: Applicance friction loss if there were some sort of equipment added to the hose
+ * EP: The highest engine pressure that the pump is producing
+ * LP: What a specific hoseline is charged to and the enginer pressure is always set to the higest line pressure
+ * 
+*/
